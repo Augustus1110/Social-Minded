8e4125104ccc5cff7e08312cc5e1ec1d3f4954dd
Augustus1110 <donahi22@gmail.com>
2023-09-08 14:49:46 -0700

    Finished user controller, added friend routes

--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -5,6 +5,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  deleteFriend
 } = require('../../controllers/userController.js');
 
 // /api/users
@@ -17,5 +19,9 @@ router
   .get(getSingleUser)
   .put(updateUser)
   .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
 
 module.exports = router;
